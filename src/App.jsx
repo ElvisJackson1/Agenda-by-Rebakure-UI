3fe2e3cb83c6ac179117630cc4779c55fedfbcf5
ElvisJackson1 <140909747+ElvisJackson1@users.noreply.github.com>
2024-03-17 13:04:56 +0200

    Agenda base UI
    Set up the front end and built the base UI.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Pages from './apps/Pages'
+
+import './App.scss'
+
+export default function App() {
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4">
+        <Pages />
+      </div>
+    </>
+  )
+}
